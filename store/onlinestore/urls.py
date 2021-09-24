902eeb1efc87ca53a88d85a33dd440c61e1f9d65
kbmaddineni <80544996+kbmaddineni@users.noreply.github.com>
2021-09-24 13:20:03 -0500

    online office supplies store
    online office supplies store

--- store/onlinestore/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.urls import path, re_path
-from django.contrib.auth import views as auth_views
-
-app_name = 'onlinestore'
-urlpatterns = [
-    path('admin/order/<int:order_id>/', views.admin_order_detail, name='admin_order_detail'),
-]
--- store/store/urls.py
+++ store/store/urls.py
@@ -18,6 +18,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('onlinestore.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
